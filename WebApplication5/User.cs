aac3fdc97c0b23e5c5ee6bceede24abe6ac80e3f
rakottvakond <rakottvakond@gmail.com>
2021-10-08 10:34:24 +0200

    h9rr

---
+++ WebApplication5/User.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace WebApplication5
+{
+    public class User
+    {
+        public Guid Guid { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Name { get; set; }
+
+        public User()
+        {
+
+        }
+    }
+}
